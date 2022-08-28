07dc6d27138eeb5bd2437ecf3f841443fbae6026
Manasi Chaudhary <mchaudh@ad.unc.edu>
2022-08-27 21:11:45 -0400

    program 01 updated

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -14,31 +14,31 @@ if len(letter) != 1:
     print("Error: Character must be a single character.")
     exit()
 
-print("Searching for",letter,"in",word)
+print("Searching for", letter, "in", word)
 
 # Check if letter is in word
 count = 0
 
 if letter == word[0]:
-    print(letter,'found at index 0')
+    print(letter, 'found at index 0')
     count += 1
 if letter == word[1]:
-    print(letter,'found at index 1')
+    print(letter, 'found at index 1')
     count += 1
 if letter == word[2]:
-    print(letter,'found at index 2')
+    print(letter, 'found at index 2')
     count += 1
 if letter == word[3]:
-    print(letter,'found at index 3')
+    print(letter, 'found at index 3')
     count += 1
 if letter == word[4]:
-    print(letter,'found at index 4')
+    print(letter, 'found at index 4')
     count += 1
 
 # Print num of instances
 if count == 0:
-    print("No instances of",letter,"found in",word)
+    print("No instances of", letter, "found in", word)
 if count == 1:
-    print("1 instance of",letter,"found in",word)
-if count >1:
-    print(count,"instances of",letter,"found in",word)
+    print("1 instance of", letter, "found in", word)
+if count > 1:
+    print(count, "instances of", letter, "found in", word)
