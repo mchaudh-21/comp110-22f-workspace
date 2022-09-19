bd6da16239c79663501b0241a0cb8212769cf07a
Manasi Chaudhary <mchaudh@ad.unc.edu>
2022-09-18 23:04:29 -0400

    ex04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,44 @@
+"""EX04 -- List Utility Functions."""
+__author__ = "730550997"
+
+
+def all(list1: list, num: int) -> bool:
+    """Function that figures out if all of the integers in a given list are the same as a given integer"""
+    index_num = 0
+    valid = True
+    while index_num < len(list1) and valid is True:
+        if num != list1[index_num]:
+            valid = False
+        index_num += 1
+
+    return valid
+
+
+def max(list2: list) -> int:
+    """Function that finds max value in list"""
+    if len(list2) == 0:
+        raise ValueError("max() arg is an empty List")
+
+    index_num = 0
+    max_num = 0
+    while index_num < len(list2):
+        if max_num < list2[index_num]:
+            max_num = list2[index_num]
+        index_num += 1
+    return max_num
+
+
+def is_equal(list1: list, list2: list) -> bool:
+    """Function that checks if two lists have deep equality """
+    # check length
+    if len(list1) != len(list2):
+        return False
+    else:
+        equal = True
+        index_num = 0
+        # check indices
+        while index_num < len(list1) and equal is True:
+            if list1[index_num] != list2[index_num]:
+                equal = False
+            index_num += 1
+    return equal
