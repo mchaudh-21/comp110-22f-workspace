10bc2cef511ea20bf64a62b2691e4f220b4bd2fb
Manasi Chaudhary <mchaudh@ad.unc.edu>
2022-09-28 17:26:14 -0400

    util3

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -6,52 +6,52 @@ from exercises.ex05.utils import only_evens, concat, sub
 
 
 # tests for only_evens
-def test_only_evens() -> None:
+def test_only_evens_one() -> None:
     """Tests edge case of empty list."""
     assert only_evens([]) == []
 
 
-#def test_only_evens() -> None:
+def test_only_evens_two() -> None:
     """Tests use case of all odd numbers."""
     list_odd = [1, 3, 5, 7]
     assert only_evens([list_odd]) == list_odd
 
 
-#def test_only_evens() -> None:
+def test_only_evens_three() -> None:
     """Tests use case of what a user input should output to."""
     user_input = [1, 2, 3, 4, 5]
     assert only_evens(user_input) == [2, 4]
 
 
 # tests for concat
-def test_concat() -> None:
+def test_concat_one() -> None:
     """Tests edge case of empty list."""
     assert concat([]) == []
 
 
-#def test_concat() -> None:
+def test_concat_two() -> None:
     """Tests use case of one empty list and a nonempty list."""
     assert concat([1, 2], []) == [1, 2]
 
 
-#def test_concat() -> None:
+def test_concat_three() -> None:
     """Test use case of possible user input and its output."""
     assert concat([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
 
 
 # tests for sub
-def test_sub() -> None:
+def test_sub_one() -> None:
     """Tests what happens if there is an empty lsit."""
     assert sub([], 1, 3) == []
 
 
-#def test_sub() -> None:
+def test_sub_two() -> None:
     """Tests use case of a negative start index."""
     example = [1, 2, 3, 4]
     assert sub(example, -2, 2) == [1, 2]
 
 
-#def test_sub() -> None:
+def test_sub_three() -> None:
     """Tests use case of out of range end index."""
     example = [1, 2, 3, 4]
     assert sub(example, 1, 9) == [2, 3, 4]
